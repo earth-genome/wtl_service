b7446bcb4b3f07e3a2fff2d6b6a690eb46cad48e
Ed Boyda <ed@space.ninja>
2018-03-13 22:36:01 -0700

    move tag_images to make structure parallel with extract_text.py
    Former-commit-id: c3124762c04747a4ac48bfa0f4b8f5da6dfd3d77

---
+++ tag_images.py
@@ -0,0 +1,35 @@
+"""Apply Watson Vision Recognition to tag images with keywords describing
+their subject matter.
+
+External function: tag_images
+""" 
+
+import watson_developer_cloud as wdc
+
+from config import WATSON_VISION_API_KEY
+
+AUTH = wdc.VisualRecognitionV3(
+    '2016-05-20',
+    api_key=WATSON_VISION_API_KEY
+)
+
+def tag_images(img_urls):
+    """Apply Watson Vision Recognition to tag images with class names.
+
+    Argument: List of urls.
+    
+    Returns:  List of dicts composed of class names and relevance scores.
+    """
+    tagslist = []
+    for url in img_urls:
+        parameters = json.dumps({'url':url})
+        try:
+            response = AUTH.classify(parameters=parameters)
+            classlist = response['images'][0]['classifiers'][0]['classes']
+            cleaned = {}
+            for c in classlist:
+                cleaned.update({c['class']: c['score']})
+            tagslist.append(cleaned)
+        except:
+            tagslist.append({})
+    return tagslist
