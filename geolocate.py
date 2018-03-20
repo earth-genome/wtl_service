ddaa5a4a92bfa3d880ed7d0ce153bb125c1493b7
Ed Boyda <ed@space.ninja>
2018-03-19 17:39:42 -0700

    add osm search; change Google lng to lon; update handling of null types
    Former-commit-id: 3dbc45d5f5e1289d4807e2878bab406daadfdfc1

--- geolocate.py
+++ geolocate.py
@@ -1,5 +1,7 @@
 # Routines to determine geographic coordinates relevant to input text
 
+#from geopy.geocoders import Nominatim
+from nominatim import nominatim
 import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
@@ -21,12 +23,36 @@ def geocode(text):
     data = requests.get(base, params=payload, verify=False).json()
 
     if data['status'] == 'OK':
-        return data['results'][0]['geometry']['location']
+        geoloc = data['results'][0]['geometry']['location']
+        # Google uses 'lng' while others (e.g. OSM) use 'lon':
+        geoloc = {'lat': geoloc['lat'], 'lon': geoloc['lng']}
+        return geoloc
     elif data['status'] == 'ZERO_RESULTS':
-        return None
+        return {}
     elif data['status'] == 'INVALID_REQUEST':
-        return None
+        return {}
     else:
         raise Exception('Geocode error: %s' % data['status'])
 
     return data
+
+def search_osm(place_name):
+    """Search osm records for place_name.
+
+    Returns: list of dicts 
+    """
+    nom = nominatim.Nominatim()
+    recs = nom.query(place_name)
+    return recs
+
+# alternate using geopy often times out:
+"""
+def search_osm(place_name):
+
+    nom = Nominatim()
+    recs = nom.geocode(place_name,
+                       exactly_one=False,
+                       addressdetails=True,
+                       geometry='geojson')
+    return recs.raw
+"""
