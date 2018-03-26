0387cf90d027ea5e87b5cc1724296e4d7847b95d
Ed Boyda <ed@space.ninja>
2018-03-25 23:46:15 -0700

    add cleaning for fb forbidden chars
    Former-commit-id: 1e93754560e83e8dddd980695a5715c191e0194b

--- tag_image.py
+++ tag_image.py
@@ -5,10 +5,12 @@ External function: get_tags
 """ 
 
 import json
+import re
 
 import watson_developer_cloud as wdc
 
 from config import WATSON_VISION_API_KEY
+from firebaseio import FB_FORBIDDEN_CHARS
 
 AUTH = wdc.VisualRecognitionV3(
     '2016-05-20',
@@ -27,6 +29,14 @@ def get_tags(img_url):
         response = AUTH.classify(parameters=parameters)
         classlist = response['images'][0]['classifiers'][0]['classes']
         tags = {c['class']:c['score'] for c in classlist}
+        tags = clean_tags(tags)
     except:
         tags = {}
     return tags
+
+def clean_tags(tags):
+    """Remove forbidden characters from tags."""
+    cleaned = {
+        re.sub(FB_FORBIDDEN_CHARS, '', k):v for k,v in tags.items()
+    }
+    return cleaned
