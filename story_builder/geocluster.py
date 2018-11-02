f2b25e49e59c2a231262f1b913abbf43fe25abcd
Edward Boyda <boyda@earthrise.media>
2018-11-02 10:17:50 -0700

    for the record (already superceded): significant rework to select a single geocoding for a location based on a match to a cluster
    Former-commit-id: fc62214d7a6fb161c574c3f28fda98a388c97864

--- story_builder/geocluster.py
+++ story_builder/geocluster.py
@@ -1,37 +1,10 @@
-"""Routines to cluster locations.
+"""Routines to cluster geographic locations.  
 
-The fundamental entity worked on by these routines is a dict, often
-taking variable name 'locations' (also sometimes 'places', if not yet
-geolocated, or 'cluster') of the following form(s):
+See geolocate.py for notes on input location data and formatting.  
 
-Initially as input:
-places = 
-{'Maputo': {'dbpedia': 'http://dbpedia.org/resource/Maputo',
-  'relevance': 0.846839,
-  'subtype': ['AdministrativeDivision', 'City']},
- 'Mozambique': {'dbpedia': '',
-  'relevance': 0.484892,
-  'subtype': ['StateOrCounty']}}
-
-Final output:
-core_locations =
-{'Maputo, Mozambique': {'address': 'Maputo, Mozambique',
-   'boundingbox': (32.5233079, -25.9839715, 32.6980592, -25.8085457),
-   'dbpedia': 'http://dbpedia.org/resource/Maputo',
-   'lat': -25.969248,
-   'lon': 32.5731746,
-   'name': 'Maputo',
-   'relevance': 0.846839,
-   'source': 'google',
-   'subtype': ['AdministrativeDivision', 'City'],
-   'types': ['locality', 'political']}}
-   
-The 'relevance' key is expected for use in scoring (selecting among) clusters.
-Other keys are allowed.
-
-That said, the base class GeoCluster is a lightweight affair which
-operates directly on latitude/longitude coordinates given in
-an (n,2) numpy array.
+The base class GeoCluster is a lightweight affair which operates
+directly on latitude/longitude coordinates given in an (n,2) numpy
+array.
 
 Simple use of this base class might entail, on input array coords:
 > gc = GeoCluster(max_dist=150, min_size=1)
@@ -39,49 +12,32 @@ Simple use of this base class might entail, on input array coords:
 > import geoclusterplot  # outside this repo
 > geoclusterplot.cluster_plot(coord_clusters, 'scatterplot.png')
 
-The descendant class GrowGeoCluster handles locations of form
-specified above, bulkier in its machinations to handle incomplete data.
-(The logic of growing a cluster with OpenStreetMap (OSM) data after
+The descendant class GrowGeoCluster handles locations dicts of form
+specified in geolocate.py, bulkier in its machinations to handle 
+tentative lat/lon assignments.
+
+The logic of growing a cluster with OpenStreetMap (OSM) data after
 seeding with Google Places API data is that the latter seems relatively
 terse in its responses, while OSM is so verbose as to be almost useless
-without some prior sense of the relevant region in which to search.)
+without some prior sense of the relevant region in which to search.
 
 Usage:
-> ggc = GrowGeoCluster(classifier=geocluster.score)
-> core_locations, clusters = ggc.seed_and_grow(places)
+> ggc = GrowGeoCluster()
+> seeds = ggc.seed(locations)
+> clusters = ggc.grow(seeds, candidated_locations)
 
-Classes:
+External classes:
     GeoCluster:  Cluster lat/lon coordinates
-    GrowGeoCluster:  Descendant class to find clusters of input places.
+    GrowGeoCluster:  Descendant class to find clusters of input locations.
 
-External functions:
-    # geocoding wrappers
-        geocode
-        dbpedia_constrained_coding
-    # Helper functions to manipulate locations dicts
-        coords_from_locations
-        locations_from_coords
-        get_centroid
-    # Functions to compute / select by distance on the Earth's surface
-        check_near
-        sort_by_distance
 
 """
 import json
-import time
 
 import geopy.distance 
 import numpy as np
 from shapely import geometry
 from sklearn.cluster import DBSCAN
-
-from story_builder import geolocate
-from utilities.geobox import geobox
-
-MAX_DIST = 150
-MIN_CLUSTER_SIZE = 1
-
-#CLASSIFIER = GrowGeoCluster()._score
 
 class GeoCluster(object):
     """Class to cluster lat/lon coordinates.
@@ -96,7 +52,7 @@ class GeoCluster(object):
         cluster: cluster input coordinates
     """
     
-    def __init__(self, max_dist=MAX_DIST, min_size=MIN_CLUSTER_SIZE):
+    def __init__(self, max_dist=150, min_size=1):
         
         self.max_dist = max_dist
         self.max_radians = max_dist/geopy.distance.EARTH_RADIUS
@@ -118,213 +74,100 @@ class GeoCluster(object):
         coord_clusters = [coords[db.labels_ == n] for n in good_labels]
         return coord_clusters
 
-    
 class GrowGeoCluster(GeoCluster):
-    """Class to geolocate and cluster named places.
-
-    (Descendant) Attributes:
-        terse_geocoder, verbose_geocoder: functions from geolocate module
-        classifier: function to score cluster quality 
-            (or None; default: self._score)
+    """Class to cluster named locations.
         
-    Public Methods:
-        __call__: Geolocate, cluster, grow and score clusters.
+    Descendant Methods:
         seed: Geolocate and cluster initial locations.
+        grow: Add places with candidate geolocations to clusters.
     """
     
-    def __init__(self,
-                 max_dist=MAX_DIST,
-                 min_size=MIN_CLUSTER_SIZE,
-                 terse_geocoder=geolocate.google_geocode,
-                 verbose_geocoder=geolocate.osm_geocode,
-                 classify=True):
-        super().__init__(max_dist, min_size)
-        self.terse_geocoder = terse_geocoder
-        self.verbose_geocoder = verbose_geocoder
-        self.classifier = None if not classify else self._score
-
-    def __call__(self, places):
-        """Geolocate, cluster, and score clusters.
-
-        Returns: list of dicts
-        """
-        good_clusters = []
-
-        # iterative geocoding
-        locations, unlocated = geocode(places, geocoder=self.terse_geocoder)
-        new_locations, unlocated = dbpedia_constrained_coding(unlocated,
-                                    geocoder=self.verbose_geocoder)
-        locations.update(new_locations)
-        third_gen, unlocated = geocode(unlocated,
-                                       geocoder=self.verbose_geocoder)
-
-        # clustering
-        clusters = self.seed(locations)
-        for cluster in clusters:
-            cluster.update(self._grow(cluster, third_gen))
-            if self.classifier:
-                if self.classifier(cluster) == 0:
-                    continue
-            good_clusters.append(cluster)
-        return self._present(good_clusters)
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
     def seed(self, locations):
-        """Cluster initial locations.
-
-        Argument locations: dict
-        
-        Returns: list of cluster dicts
-        """
+        """Cluster locations. Returns a list of subdicts of locations."""
         try: 
             coords = coords_from_locations(locations)
         except ValueError as e:
             raise ValueError('No seed coordinates found.') 
         coord_clusters = super().cluster(coords)
-        clusters = [locations_from_coords(coord_cluster, locations)
-                    for coord_cluster in coord_clusters]
+        seeds = [locations_from_coords(cc, locations) for cc in coord_clusters]
+        return seeds
+
+    def grow(self, clusters, candidated):
+        """Add places with candidate geolocations to clusters.
+
+        The routine adds to a cluster the candidate locations which are
+        sufficiently close to an existing cluster. Since geolocations in 
+        candidated places are ranked, the first viable cluster placement is 
+        accepted and considered thenceforth as the resolved geolocation for 
+        that place.
+
+        Arguments:
+            clusters: list of dicts of locations
+            candidated: dict with lists of geolocation 'candidates' in values
+        
+        Returns: updated list of dicts of locations
+        """
+        clusters = json.loads(json.dumps(clusters))
+        for name, data in candidated.items():
+            candidates = data.pop('candidates')
+            for geoloc in candidates:
+                cluster_idx = self._match_to_cluster(geoloc, clusters)
+                if cluster_idx is not None:
+                    clusters[cluster_idx].update({name: dict(data, **geoloc)})
+                    break
+            else:
+                clusters.append({name: dict(data, **candidates[0])})
         return clusters
 
-    def _grow(self, cluster, recodes):
-        """Add preliminarily unlocated places to cluster.
+    def _match_to_cluster(self, geolocation, clusters):
+        """Attempt to match geolocation to a proximal cluster.
 
-        The routine adds to cluster the recoded locations which are within
-        self.max_dist of an existing cluster point.
+        A geolocation is considered to match to a cluster if it either 
+        intersects one of the cluster's locations or if its lat, lon
+        is within self.max_dist of the lat, lon of one of the cluster's
+        locations. The first cluster to match is accepted.
 
-        Arguments
-            cluster: dict of locations
-            recodes: dict of locations
-        
-        Returns: subdict of recoded locations to update cluster
+        Arguments:
+            geolocation: dict with 'lat', 'lon' as keys
+            clusters: list of dicts of locations
+
+        Returns: List index of matched cluster, if available, or None
         """
-        new_locations = {}
-        for name, data in recodes.items():
-            if check_near(data, cluster, max_dist=self.max_dist):
-                new_locations.update({name: data})
-        return new_locations
+        for n, cluster in enumerate(clusters):
+            if len(cluster) > 0:
+                if (self._check_intersects(geolocation, cluster) or
+                    self._check_near(geolocation, cluster)):
+                    return n
+        return
 
-    # WIP: ad hoc cluster scoring to be replaced with a classifier
-    def _score(self, cluster):
-        """Ad hoc cluster scoring."""
-        SCORE_THRESH = .5
-        SIZE_THRESH = 40 # km
-        unique_names, relevance_scores, sizes = [], [], []
+    def _check_intersects(self, geolocation, cluster):
+        """Check whether geolocation intersects any location in cluster."""
+        try: 
+            source = geometry.box(*geolocation['boundingbox'])
+        except KeyError:
+            source = geometry.Point(geolocation['lon'], geolocation['lat'])
         for data in cluster.values():
             try:
-                size = geobox.get_side_distances(
-                    geometry.box(*data['boundingbox']))
-                sizes.append(np.mean(size))
+                target = geometry.box(*data['boundingbox'])
             except KeyError:
-                pass
-            if data['name'] in unique_names:
-                continue
-            try:
-                relevance_scores.append(data['relevance'])
-                unique_names.append(data['name'])
-            except KeyError:
-                pass
-        min_size = np.min(sizes)
-        score = len(unique_names) * np.mean(relevance_scores)
-        return score if score > SCORE_THRESH and min_size < SIZE_THRESH else 0
-    
-    def _present(self, clusters):
-        """Record cluster metadata and flatten clusters to a dict of locations.
+                target = geometry.Point(data['lon'], data['lat'])
+            if source.intersection(target).bounds:
+                return True
+        return False
 
-        Argument clusters: list of dicts
-
-        Returns: dict of locations, list of cluster metadata
-        """
-        metadata = []
-        for cluster in clusters:
-            metadata.append({
-                'centroid': get_centroid(cluster),
-                'locations': [name for name in cluster.keys()]
-            })
-            if self.classifier:
-                metadata[-1].update({
-                    'score': self.classifier(cluster)
-                })
-        locations = {name:data for cluster in clusters
-                     for name, data in cluster.items()}
-        return locations, metadata
-    
-
-# Geocoding wrappers
-
-def geocode(places, geocoder):
-    """Find geo-coordinates for input places.
-
-    Arguments:
-        places: dict with place names as keys
-        geocoder: Function from geolocate module
-
-    Returns: dicts of places located and unlocated
-    """
-    located, unlocated = {}, {}
-    for name, data in places.items():
-        try: 
-            geolocs = geocoder(name)
-        except Exception as e:
-            print('Geocoding {}: {}'.format(name, repr(e)))
-            geolocs = []
-        if not geolocs:
-            unlocated.update({
-                name: {
-                    'name': name,
-                    **data
-                }
-            })
-        else:
-            for geoloc in geolocs:
-                located.update({
-                    geoloc['address']: {
-                        'name': name,
-                        **data,
-                        **geoloc
-                    }
-                })
-    return located, unlocated
-
-def dbpedia_constrained_coding(places, geocoder):
-    """Use a dbpedia reference to constrain a search with (verbose) geocoder.
-
-    Arguments:
-        places: dict with place names as keys
-        geocoder: Function from geolocate module
-
-    Returns: dicts of places located and unlocated
-    """
-    located, unlocated = {}, {}
-    for name, data in places.items():
-        try:
-            target = geolocate.dbpedia_geocode(data['dbpedia'])
-        except KeyError:
-            target = {}
-        if not target:
-            unlocated.update({
-                name: {
-                    'name': name,
-                    **data
-                }
-            })
-        else:
-            lat, lon = target['lat'], target['lon']
-            geolocs = geocode({name: data}, geocoder)[0]
-            if len(geolocs) > 0: 
-                nearest = next(iter(sort_by_distance(geolocs, lat, lon)))[0]
-                box = geometry.box(*list(nearest.values())[0]['boundingbox'])
-                if box.intersects(geometry.Point(lon, lat)): 
-                    located.update(nearest)
-            else:
-                located.update({
-                    target['address']: {
-                        'name': name,
-                        **data,
-                        **target
-                    }
-                })
-    return located, unlocated
-
-    
+    def _check_near(self, geolocation, cluster):
+        """Check that geolocation is withing max_dist of a cluster point."""
+        lat, lon = geolocation['lat'], geolocation['lon']
+        coord_cluster = coords_from_locations(cluster)
+        for target_coords in coord_cluster:
+            dist = geopy.distance.great_circle((lat, lon), target_coords)
+            if dist.kilometers <= self.max_dist:
+                return True
+        return False
+        
 # Helper functions to manipulate locations dicts
     
 def coords_from_locations(locations):
@@ -343,7 +186,7 @@ def coords_from_locations(locations):
     return np.array(coords)
 
 def locations_from_coords(coords, locations):
-    """Find locations dict items for given array of lat/lon coords.
+    """Find locations dict items corresponding to given array of lat/lon coords.
 
     This routine inverts coords_from_locations(). It seeks an exact
     lat/lon match between elements of coords and locations.
@@ -352,7 +195,7 @@ def locations_from_coords(coords, locations):
         coords: numpy array of lat/lon(s), shape (n,2).
         locations: dict
 
-    Returns: dict of locations
+    Returns: subdict of locations
     """
     good_locations = {}
     for name, data in locations.items():
@@ -362,59 +205,3 @@ def locations_from_coords(coords, locations):
         except KeyError:
             pass
     return good_locations
-
-def get_centroid(cluster):
-    """Find centroid of cluster points.
-
-    Argument cluster: dict of locations
-
-    Returns: dict with lat/lon
-    """
-    # WIP: redo after harmonizing google/osm locations
-    points = []
-    for data in cluster.values():
-        try:
-            points.append((data['lon'], data['lat']))
-        except KeyError:
-            pass
-    centroid = geometry.MultiPoint(points).centroid
-    return {'lat': centroid.y, 'lon': centroid.x}
-
-# Functions to compute / select by distance on the Earth's surface
-
-def check_near(location, cluster, max_dist=MAX_DIST):
-    """Check that location is withing max_dist of cluster.
-
-    Arguments:
-        location: dict with 'lat', 'lon' as keys
-        cluster: dict of locations
-        max_dist: max accpetable distance form any point in the cluster
-            in km
-
-    Returns:  True if within max_distance else False
-    """
-    lat, lon = location['lat'], location['lon']
-    coord_cluster = coords_from_locations(cluster)
-    for target_coords in coord_cluster:
-        dist = geopy.distance.great_circle((lat, lon), target_coords)
-        if dist.kilometers <= max_dist:
-            return True
-    return False
-
-def sort_by_distance(locations, lat, lon):
-    """Sort locations by least distance from lat, lon.
-
-    Arguments:
-        locations: dict
-        lat, lon type: float
-
-    Returns: list of tuples of locations and distances
-    """
-    loc_distances = []
-    for name, data in locations.items():
-        dist = geopy.distance.great_circle((data['lat'], data['lon']),
-                                           (lat, lon))
-        loc_distances.append(({name: data.copy()}, dist.kilometers))
-    loc_distances.sort(key=lambda d: d[1])
-    return loc_distances
-
--- story_builder/geolocate.py
+++ story_builder/geolocate.py
@@ -1,149 +1,137 @@
-"""Routines to determine geographic coordinates relevant to input text.
+"""Routines to geocode and score relevance of locations to satellite-based 
+reporting. 
 
-External functions:
-    google_geocode(text)
-    osm_geocode(text)
-    dbpedia_geocode(dpbedia_url)
-    
-If a query returns multiple records, records are filtered (deleted) for
-intersection with earlier records in the list.
+The fundamental entity worked on by these routines is a dict, often
+taking variable name 'locations' (also sometimes 'places', if not yet
+geocoded, or 'cluster') of the following form(s):
+
+# TODO: update these examples:
+
+Initially as input:
+places = 
+{'Maputo': {'dbpedia': 'http://dbpedia.org/resource/Maputo',
+  'relevance': 0.846839,
+  'subtype': ['AdministrativeDivision', 'City']},
+ 'Mozambique': {'dbpedia': '',
+  'relevance': 0.484892,
+  'subtype': ['StateOrCounty']}}
+
+Final output:
+core_locations =
+{'Maputo, Mozambique': {'address': 'Maputo, Mozambique',
+   'boundingbox': (32.5233079, -25.9839715, 32.6980592, -25.8085457),
+   'dbpedia': 'http://dbpedia.org/resource/Maputo',
+   'lat': -25.969248,
+   'lon': 32.5731746,
+   'name': 'Maputo',
+   'relevance': 0.846839,
+   'source': 'google',
+   'subtype': ['AdministrativeDivision', 'City'],
+   'types': ['locality', 'political']}}
+   
+The 'relevance', 'text', and 'mentions' keys are required. 'Mentions' can be
+extracted with the find_mentions function, included in this module because
+the limit parameter impacts classifier architecture.
+
+External class: Geolocate
+External function: find_mentions
+
+# TODO: add usage
+Usage from 
+
 """
 
-import re
+import functools
 
-from geopy import geocoders
-import requests
-from requests.packages.urllib3.exceptions import InsecureRequestWarning
-from shapely import geometry
+import nltk
 
-from config import GOOGLE_GEO_API_KEY
-from utilities.geobox import geobox
-from utilities.firebaseio import FB_FORBIDDEN_CHARS
+from story_builder import geocode
+from story_builder import geocluster
 
-requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+def find_mentions(place, text, limit=6):
+    """Extract sentences where place is mentioned in text.
     
-def google_geocode(text):
-    """Accepts a text query and returns lat-lon coordinates and
-    (if possible) a bounding box.
-    """
-    base = 'https://maps.googleapis.com/maps/api/place/textsearch/json'
-    payload = {
-		'query': text,
-		'key': GOOGLE_GEO_API_KEY
-    }
-    data = requests.get(base, params=payload, verify=False).json()
-    
-    if data['status'] == 'OK':
-        cleaned = []
-        for d in data['results']:
-            try:
-                address = ', '.join((d['name'], d['formatted_address']))
-            except KeyError:
-                address = d['name']
-            geom = d['geometry']
-            try: 
-                bounds = geobox.google_to_shapely_box(geom['viewport']).bounds
-            except KeyError:
-                bounds = ()
-            if _check_intersects(bounds, cleaned):
-                continue
-            # Google uses 'lng' while others (e.g. OSM) use 'lon':
-            geoloc = {
-                'source': 'google',
-                'address': _clean_address(address),
-                'types': d['types'],
-                'lat': geom['location']['lat'],
-                'lon': geom['location']['lng'],
-                'boundingbox': bounds
-            }
-            cleaned.append(geoloc)
-        return cleaned
-    elif data['status'] == 'ZERO_RESULTS':
-        return []
-    elif data['status'] == 'INVALID_REQUEST':
-        return []
-    else:
-        raise Exception('Geocode error: %s' % data['status'])
-    return
-
-
-def osm_geocode(place_name):
-    """Search osm records for place_name.
-
-    Returns: list of dicts 
-    """
-    nom = geocoders.Nominatim(user_agent='Earthrise.media')
-    recs = nom.geocode(place_name,
-                       exactly_one=False,
-                       addressdetails=False,
-                       limit=20)
-    if recs is None:
-        return []
-    else:
-        recs = [r.raw for r in recs]
-    cleaned = []
-    for r in recs:
-        bounds = geobox.osm_to_shapely_box(r['boundingbox']).bounds
-        if _check_intersects(bounds, cleaned):
-            continue
-        geoloc = {
-            'source': 'osm',
-            'address': _clean_address(r['display_name']),
-            'types': [r['type']],
-            'lat': float(r['lat']),
-            'lon': float(r['lon']),
-            'boundingbox': bounds
-        }
-        cleaned.append(geoloc)
-    return cleaned
-
-
-def dbpedia_geocode(url):
-    """Extract lat/lon from dbpedia url.
-
-    Arugment url should take the form:
-        'http://dbpedia.org/resource/Kathmandu'
-    """
-    latkey = 'http://www.w3.org/2003/01/geo/wgs84_pos#lat'
-    lonkey = 'http://www.w3.org/2003/01/geo/wgs84_pos#long'
-    
-    jsonbase = 'http://dbpedia.org/data/'
-    entity = url.split('resource/')[-1]
-    jsonurl = jsonbase + entity + '.json'
-    data = requests.get(jsonurl).json()
-    try:
-        lat = data[url][latkey][0]['value']
-        lon = data[url][lonkey][0]['value']
-    except KeyError:
-        return {}
-    geoloc = {
-        'source': 'dbpedia',
-        'address': _clean_address(re.sub('_', ' ', entity)),
-        'lat': lat,
-        'lon': lon,
-    }
-    return geoloc
-
-def _check_intersects(bounds, geolocs):
-    """Check if coordinate bounds intersect any bounding boxes in geolocs.
-
     Arguments:
-        bounds: list or tuple of coordinates
-        geolocs: list of dicts with 'boundingbox' as key
-        
-    Returns: True/False
-    """
-    bbox = geometry.box(*bounds)
-    query_boxes = []
-    for g in geolocs:
-        try:
-            query_boxes.append(geometry.box(*g['boundingbox']))
-        except KeyError:
-            query_boxes.append(geometry.Point(g['lon'], g['lat']))
-    for qb in query_boxes:
-        if bbox.intersects(qb):
-            return True
-    return False
+        place: string to find in sentences of text
+        text: long string to be split into sentences
 
-def _clean_address(address):
-    return re.sub(FB_FORBIDDEN_CHARS, '', address)
+    Returns: list of sentences
+    """
+    sentences = nltk.sent_tokenize(text)
+    mentions = [s for s in sentences if place in s]
+    return mentions[:limit]
+
+class Geolocate(object):
+    """Class to geolocate and score locations.
+
+    Attributes:
+        terse_geocoder, verbose_geocoder: functions from geocode module
+        cluster_tool: instance of GrowGeoCluster class
+        classifier: TODO
+
+    External methods: 
+        __call__: Geocode, cluster, and score input places.
+        iterative_geocode: Find geo-coordinates with terse and verbose 
+            geocoders in sequence.
+        find_mentions: Find setences where place is mentioned in a text.
+            
+    """
+    def __init__(self, 
+                 terse_geocoder=functools.partial(
+                     geocode.google_geocode, N_records=1),
+                 verbose_geocoder=functools.partial(
+                     geocode.osm_geocode, N_records=20),
+                 cluster_tool=geocluster.GrowGeoCluster(),
+                 classifier=None):
+        self.terse_geocoder = terse_geocoder
+        self.verbose_geocoder = verbose_geocoder
+        self.cluster_tool = cluster_tool
+        self.classifier = classifier
+
+    def __call__(self, places):
+        """Geocode, cluster, and score input places."""
+        locations, candidated = self.iterative_geocode(places)
+        
+        seeds = self.cluster_tool.seed(locations)
+        grown = self.cluster_tool.grow(seeds, candidated)
+
+        good_locations = {}
+        for cluster in grown:
+            for name, data in cluster.items():
+                data.update({'cluster': list(cluster.keys())})
+                # TODO: add classifier
+                data.update({'score': None})
+                good_locations.update({name: data})
+
+        return good_locations
+        
+    def iterative_geocode(self, places):
+        """Find geo-coordinates with terse and verbose geocoders in sequence.
+
+        Returns: dicts of places uniquely located and those with multiple 
+            candidates
+       """
+        locations, _, unlocated = self._bulk_geocode(
+            places, geocoder=self.terse_geocoder)
+        new_locations, candidated, _ = self._bulk_geocode(
+            unlocated, geocoder=self.verbose_geocoder)
+        locations.update(**new_locations)
+        return locations, candidated
+
+    def _bulk_geocode(self, places, geocoder):
+        """Find geo-coordinates for input places."""
+        located, candidated, unlocated = {}, {}, {}
+        for name, data in places.items():
+            try: 
+                geolocs = geocoder(name)
+            except Exception as e:
+                print('Geocoding {}: {}'.format(name, repr(e)), flush=True)
+                geolocs = []
+            if len(geolocs) == 0:
+                unlocated.update({name: data})
+            elif len(geolocs) == 1:
+                located.update({name: dict(geolocs[0], **data)})
+            else:
+                candidated.update({name: dict({'candidates': geolocs}, **data)})
+
+        return located, candidated, unlocated
